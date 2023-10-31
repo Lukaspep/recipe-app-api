bfca95f49b5f33b117160ed6a207766e5da616b5
Lukaspep <lucasbernasconi51@gmail.com>
2023-10-31 19:10:16 -0300

    Configured docker compose and checks to wait for db.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,13 @@
 Calculator functions
 """
 
+
 def add(x, y):
-    """Add x and y and return the result."""
+    """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
-    """Subtract y from x and return the result."""
-    return x - y
+    """Subtract x from y and return result."""
+    return y - x
 
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,26 +1,28 @@
 """
-Django command to pause execution until the database is available
+Django command to wait for the database to be available.
 """
 import time
+
 from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """Django command to wait for the database"""
-         
+    """Django command to wait for database."""
+
     def handle(self, *args, **options):
-        """Entrypoint for the command"""
-        self.stdout.write("Waiting for the database...")
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write("Database unavailable, waiting 1 second...")
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
-        self.stdout.write(self.style.SUCCESS("Database is available!"))
 
-        
+        self.stdout.write(self.style.SUCCESS('Database available!'))
+
